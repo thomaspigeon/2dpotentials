d9e5c8509d9bb3a325d7e2a8fe2cdba111555c56
Thomas Pigeon <tpigeon@pl1101-pro.paris.inria.fr>
2023-03-20 09:18:31 +0100

    Create the various AE models used

---
+++ autoencoders/ae_models.py
@@ -0,0 +1,142 @@
+import torch
+
+class DeepAutoEncoder(torch.nn.Module):
+    """Class for classic auto-encoders"""
+    def __init__(self, encoder_dims, decoder_dims, dropout):
+        """Initialise auto encoder with hyperbolic tangent activation function
+
+        :param encoder_dims:    list, List of dimensions for encoder, including input/output layers
+        :param decoder_dims:    list, List of dimensions for decoder, including input/output layers
+        :param dropout:         int, value of the dropout probability
+        """
+        super(DeepAutoEncoder, self).__init__()
+        layers = []
+        for i in range(len(encoder_dims) - 2):
+            layers.append(torch.nn.Linear(encoder_dims[i], encoder_dims[i + 1]))
+            layers.append(torch.nn.Dropout(dropout))
+            layers.append(torch.nn.Tanh())
+        layers.append(torch.nn.Linear(encoder_dims[-2], encoder_dims[-1]))
+        self.encoder = torch.nn.Sequential(*layers)
+
+        layers = []
+        for i in range(len(decoder_dims) - 2):
+            layers.append(torch.nn.Linear(decoder_dims[i], decoder_dims[i + 1]))
+            layers.append(torch.nn.Dropout(dropout))
+            layers.append(torch.nn.Tanh())
+        layers.append(torch.nn.Linear(decoder_dims[-2], decoder_dims[-1]))
+        self.decoder = torch.nn.Sequential(*layers)
+
+    def forward(self, inp):
+        encoded = self.encoder(inp)
+        decoded = self.decoder(encoded)
+        return decoded
+
+
+
+class DeepAutoEncoderDoubleDec(torch.nn.Module):
+    """Class for auto-encoders with two decoders. This class does not contain a forward function."""
+    def __init__(self, encoder_dims, decoder_dims, dropout):
+        """Initialise auto encoder with hyperbolic tangent activation function
+
+        :param encoder_dims:    list, List of dimensions for encoder, including input/output layers
+        :param decoder_dims:    list, List of dimensions for decoder, including input/output layers
+        :param dropout:         int, value of the dropout probability
+        """
+        super(DeepAutoEncoderDoubleDec, self).__init__()
+        layers = []
+        for i in range(len(encoder_dims) - 2):
+            layers.append(torch.nn.Linear(encoder_dims[i], encoder_dims[i + 1]))
+            layers.append(torch.nn.Dropout(dropout))
+            layers.append(torch.nn.Tanh())
+        layers.append(torch.nn.Linear(encoder_dims[-2], encoder_dims[-1]))
+
+        self.encoder = torch.nn.Sequential(*layers)
+
+        layers = []
+        for i in range(len(decoder_dims) - 2):
+            layers.append(torch.nn.Linear(decoder_dims[i], decoder_dims[i + 1]))
+            layers.append(torch.nn.Dropout(dropout))
+            layers.append(torch.nn.Tanh())
+        layers.append(torch.nn.Linear(decoder_dims[-2], decoder_dims[-1]))
+
+        self.decoder1 = torch.nn.Sequential(*layers)
+
+        layers = []
+        for i in range(len(decoder_dims) - 2):
+            layers.append(torch.nn.Linear(decoder_dims[i], decoder_dims[i + 1]))
+            layers.append(torch.nn.Dropout(dropout))
+            layers.append(torch.nn.Tanh())
+        layers.append(torch.nn.Linear(decoder_dims[-2], decoder_dims[-1]))
+
+        self.decoder2 = torch.nn.Sequential(*layers)
+
+    def decoded1(self, inp):
+        enc = self.encoder(inp)
+        dec = self.decoder1(enc)
+        return dec
+
+    def decoded2(self, inp):
+        enc = self.encoder(inp)
+        dec = self.decoder2(enc)
+        return dec
+
+class EffectiveDynamicsAutoEncoder(torch.nn.Module):
+    """Class for auto-encoders with effective dynamics. """
+    def __init__(self, encoder_dims, decoder_dims, bias_dims, diff_dims, dropout):
+        """Initialise auto encoder with hyperbolic tangent activation function
+
+        :param encoder_dims:    list, List of dimensions for encoder, including input/output layers
+        :param decoder_dims:    list, List of dimensions for decoder, including input/output layers
+        :param bias_dims:       list, List of dimensions for the decoder encoding the bias of the effective dynamics,
+                                including input/output layers
+        :param diff_dims:       list, List of dimensions for the decoder encoding the bias of the effective dynamics,
+                                including input/output layers
+        :param dropout:         int, value of the dropout probability
+        """
+        super(EffectiveDynamicsAutoEncoder, self).__init__()
+        layers = []
+        for i in range(len(encoder_dims) - 2):
+            layers.append(torch.nn.Linear(encoder_dims[i], encoder_dims[i + 1]))
+            layers.append(torch.nn.Dropout(dropout))
+            layers.append(torch.nn.Tanh())
+        layers.append(torch.nn.Linear(encoder_dims[-2], encoder_dims[-1]))
+        self.encoder = torch.nn.Sequential(*layers)
+
+        layers = []
+        for i in range(len(decoder_dims) - 2):
+            layers.append(torch.nn.Linear(decoder_dims[i], decoder_dims[i + 1]))
+            layers.append(torch.nn.Dropout(dropout))
+            layers.append(torch.nn.Tanh())
+        layers.append(torch.nn.Linear(decoder_dims[-2], decoder_dims[-1]))
+        self.decoder = torch.nn.Sequential(*layers)
+
+        layers = []
+        for i in range(len(bias_dims) - 2):
+            layers.append(torch.nn.Linear(bias_dims[i], bias_dims[i + 1]))
+            layers.append(torch.nn.Dropout(dropout))
+            layers.append(torch.nn.Tanh())
+        layers.append(torch.nn.Linear(bias_dims[-2], bias_dims[-1]))
+        self.ed_b = torch.nn.Sequential(*layers)
+
+        layers = []
+        for i in range(len(diff_dims) - 2):
+            layers.append(torch.nn.Linear(diff_dims[i], diff_dims[i + 1]))
+            layers.append(torch.nn.Dropout(dropout))
+            layers.append(torch.nn.Tanh())
+        layers.append(torch.nn.Linear(diff_dims[-2], diff_dims[-1]))
+        self.ed_d = torch.nn.Sequential(*layers)
+
+    def forward(self, inp):
+        encoded = self.encoder(inp)
+        decoded = self.decoder(encoded)
+        return decoded
+
+    def ed_bias(self, enc):
+        ed_bias = self.ed_b(enc)
+        ed_bias = torch.reshape(ed_bias, (ed_bias.shape + (1,)))
+        return ed_bias
+
+    def ed_diff(self, enc):
+        ed_diff = self.ed_d(enc)
+        ed_diff = torch.reshape(ed_diff, ed_diff.shape + (ed_diff.shape[-1],))
+        return ed_diff
