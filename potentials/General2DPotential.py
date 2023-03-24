89e44cc82a2ab92d70cc9f0021732288555fbfcb
Thomas Pigeon <tpigeon@pl1101-pro.paris.inria.fr>
2023-03-24 17:01:42 +0100

    correct typos

--- potentials/General2DPotential.py
+++ potentials/General2DPotential.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class General2DPotential:
     """Class containing useful functions for all 2D potentials.
 
@@ -14,9 +15,10 @@ class General2DPotential:
         :param R_radius:         float, radius of the disc defining the R state
         :param minP:             np.array with ndim==2, shape==[1,2], center of the disc defining the P state
         :param P_radius:         float, radius of the disc defining the P state
-        :param dx:               step for the gradient descent to compute minimum energy path starting from first order saddle
-                          point
-        :param saddle_points:    list, len==any, list of np.array with ndim==2, shape==[1,2], list of first order saddle points
+        :param dx:               step for the gradient descent to compute minimum energy path starting from first order
+                                 saddle point
+        :param saddle_points:    list, len==any, list of np.array with ndim==2, shape==[1,2], list of first order saddle
+                                 points
         :param x_domain:         list of two int, min and max value of x for the plots
         :param y_domain:         list of two int, min and max value of y for the plots
         :param V_domain:         list of two int, min and max value of the potential for the heat maps plot
@@ -73,13 +75,15 @@ class General2DPotential:
     def set_SigmaP_position(self, SigmaP_position):
         """Allows to reset the position of SigmaP
 
-        :param SigmaP_position:  float, the factor of P_radius defining the position of SigmaP, it should be >= 1"""
+        :param SigmaP_position:  float, the factor of P_radius defining the position of SigmaP, it should be >= 1
+        """
         self.SigmaP_position = SigmaP_position
     def in_R(self, x):
         """Definition of the Reactant state R
 
         :param x: np.array, ndim==2, shape==[any, 2]
-        :returns bool (if x.ndim == 1) or array of bool (if x.ndim == 2) Whether the argument x is inside state R """
+        :returns bool (if x.ndim == 1) or array of bool (if x.ndim == 2) Whether the argument x is inside state R
+        """
         if x.ndim == 1:
             return np.sqrt(np.sum((x - self.minR) ** 2)) < self.R_radius
         elif x.ndim == 2:
@@ -89,7 +93,8 @@ class General2DPotential:
         """Test if a point is above the surface Sigma_R enclosing the state R
 
         :param x: np.array, ndim==2, shape==[any, 2]
-        :returns bool (if x.ndim == 1) or array of bool (if x.ndim == 2) Whether the argument x is inside above SigmaR """
+        :returns bool (if x.ndim == 1) or array of bool (if x.ndim == 2) Whether the argument x is inside above SigmaR
+        """
         if x.ndim == 1:
             return np.sqrt(np.sum((x - self.minR)**2)) >= self.SigmaR_position * self.R_radius
         elif x.ndim == 2:
@@ -99,7 +104,8 @@ class General2DPotential:
         """Definition of the Reactant state P
 
         :param x: np.array, ndim==2, shape==[any, 2]
-        :returns bool (if x.ndim == 1) or array of bool (if x.ndim == 2) Whether the argument x is inside state P """
+        :returns bool (if x.ndim == 1) or array of bool (if x.ndim == 2) Whether the argument x is inside state P
+        """
         if x.ndim == 1:
             return np.sqrt(np.sum((x - self.minP) ** 2)) < self.P_radius
         elif x.ndim == 2:
@@ -109,7 +115,8 @@ class General2DPotential:
         """Test if a point is above the surface Sigma_R enclosing the state R
 
         :param x: np.array, ndim==2, shape==[any, 2]
-        :returns bool (if x.ndim == 1) or array of bool (if x.ndim == 2) Whether the argument x is inside above SigmaP"""
+        :returns bool (if x.ndim == 1) or array of bool (if x.ndim == 2) Whether the argument x is inside above SigmaP
+        """
         if x.ndim == 1:
             return np.sqrt(np.sum((x - self.minP)**2)) >= self.SigmaP_position * self.P_radius
         elif x.ndim == 2:
@@ -147,7 +154,9 @@ class General2DPotential:
         gridy = np.linspace(self.y_domain[0], self.y_domain[1], self.n_bins_y)
         self.x_plot = np.outer(gridx, np.ones(self.n_bins_y))
         self.y_plot = np.outer(gridy, np.ones(self.n_bins_x)).T
-        self.x2d = np.concatenate((self.x_plot.reshape(self.n_bins_x * self.n_bins_y, 1), self.y_plot.reshape(self.n_bins_x * self.n_bins_y, 1)), axis=1)
+        self.x2d = np.concatenate((self.x_plot.reshape(self.n_bins_x * self.n_bins_y, 1),
+                                   self.y_plot.reshape(self.n_bins_x * self.n_bins_y, 1)),
+                                  axis=1)
 
     def plot_potential_heat_map(self, ax, set_lim=True):
         """Plot the potential heat map to the given ax
@@ -157,7 +166,13 @@ class General2DPotential:
         if set_lim:
             ax.set_ylim(self.y_domain[0], self.y_domain[1])
             ax.set_xlim(self.x_domain[0], self.x_domain[1])
-        ax.pcolormesh(self.x_plot, self.y_plot, pot_on_grid, cmap='coolwarm', shading='auto', vmin=self.V_domain[0], vmax=self.V_domain[0])
+        ax.pcolormesh(self.x_plot,
+                      self.y_plot,
+                      pot_on_grid,
+                      cmap='coolwarm',
+                      shading='auto',
+                      vmin=self.V_domain[0],
+                      vmax=self.V_domain[0])
 
     def plot_function_iso_levels(self, ax, function, n_lines, set_lim=True):
         """Plot the iso-lines of a given function to the given ax
@@ -171,5 +186,3 @@ class General2DPotential:
             ax.set_ylim(self.y_domain[0], self.y_domain[1])
             ax.set_xlim(self.x_domain[0], self.x_domain[1])
         ax.contour(self.x_plot, self.y_plot, function_on_grid, n_lines, cmap='viridis')
-
-
